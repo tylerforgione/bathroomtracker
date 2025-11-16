c2558e81360766711247790e9e3b4dc69632ea99
Léa Abdel Malak <leaabdelmalak@gmail.com>
2025-11-16 10:40:29 -0500

    fixed database + urinals

--- frontend/src/data/buildings.js
+++ frontend/src/data/buildings.js
@@ -6,32 +6,32 @@ const buildings = [
     lng: -73.578850,
     floors: {
       "0": {
-        men: { status: "ok", rating: 4.2, stalls: 3 },
+        men: { status: "ok", rating: 4.2, stalls: 3, urinals: 4 },
         women: { status: "broken", rating: 4.0, stalls: 4 },
         gn: { status: "cleaning", rating: 4.4, stalls: 1 }
       },  
       "1": {
-        men: { status: "ok", rating: 4.2, stalls: 3 },
+        men: { status: "ok", rating: 4.2, stalls: 3, urinals: 4  },
         women: { status: "broken", rating: 4.0, stalls: 4 },
         gn: { status: "cleaning", rating: 4.4, stalls: 1 }
       },
       "2": {
-        men: { status: "ok", rating: 4.5, stalls: 2 },
+        men: { status: "ok", rating: 4.5, stalls: 2, urinals: 4  },
         women: { status: "ok", rating: 4.7, stalls: 4 },
         gn: { status: "ok", rating: 4.8, stalls: 1 }
       },
       "3": {
-        men: { status: "ok", rating: 4.2, stalls: 3 },
+        men: { status: "ok", rating: 4.2, stalls: 3, urinals: 4  },
         women: { status: "broken", rating: 4.0, stalls: 4 },
         gn: { status: "cleaning", rating: 4.4, stalls: 1 }
       },
       "4": {
-        men: { status: "ok", rating: 4.2, stalls: 3 },
+        men: { status: "ok", rating: 4.2, stalls: 3, urinals: 4  },
         women: { status: "broken", rating: 4.0, stalls: 4 },
         gn: { status: "cleaning", rating: 4.4, stalls: 1 }
       },
       "5": {
-        men: { status: "ok", rating: 4.2, stalls: 3 },
+        men: { status: "ok", rating: 4.2, stalls: 3, urinals: 4  },
         women: { status: "broken", rating: 4.0, stalls: 4 },
         gn: { status: "cleaning", rating: 4.4, stalls: 1 }
       }
@@ -45,32 +45,32 @@ const buildings = [
     lng: -73.576411,
     floors: {
       "Basement": {
-        men: { status: "ok", rating: 4.2, stalls: 3 },
+        men: { status: "ok", rating: 4.2, stalls: 3, urinals: 4  },
         women: { status: "broken", rating: 4.0, stalls: 4 },
         gn: { status: "cleaning", rating: 4.4, stalls: 1 }
       },
       "1": {
-        men: { status: "ok", rating: 4.5, stalls: 2 },
+        men: { status: "ok", rating: 4.5, stalls: 2, urinals: 4  },
         women: { status: "ok", rating: 4.7, stalls: 3 },
         gn: { status: "ok", rating: 4.8, stalls: 1 }
       },
       "2": {
-        men: { status: "ok", rating: 4.5, stalls: 2 },
+        men: { status: "ok", rating: 4.5, stalls: 2, urinals: 4  },
         women: { status: "ok", rating: 4.7, stalls: 3 },
         gn: { status: "ok", rating: 4.8, stalls: 1 }
       },
       "3": {
-        men: { status: "ok", rating: 4.2, stalls: 3 },
+        men: { status: "ok", rating: 4.2, stalls: 3, urinals: 4  },
         women: { status: "broken", rating: 4.0, stalls: 4 },
         gn: { status: "cleaning", rating: 4.4, stalls: 1 }
       },
       "4": {
-        men: { status: "ok", rating: 4.2, stalls: 3 },
+        men: { status: "ok", rating: 4.2, stalls: 3, urinals: 4  },
         women: { status: "broken", rating: 4.0, stalls: 4 },
         gn: { status: "cleaning", rating: 4.4, stalls: 1 }
       },
       "5": {
-        men: { status: "ok", rating: 4.2, stalls: 3 },
+        men: { status: "ok", rating: 4.2, stalls: 3, urinals: 4  },
         women: { status: "broken", rating: 4.0, stalls: 4 },
         gn: { status: "cleaning", rating: 4.4, stalls: 1 }
       }
@@ -84,28 +84,27 @@ const buildings = [
     lng: -73.576446,
     floors: {
       "Basement": {
-        men: { status: "ok", rating: 4.2, stalls: 3 },
-        women: { status: "broken", rating: 4.0, stalls: 4 },
+        men: { status: "ok", rating: 3.2, stalls: 3, urinals: 4  },
+        women: { status: "broken", rating: 4.0, stalls: 8 },
         gn: { status: "cleaning", rating: 4.4, stalls: 1 }
       },
       "1": {
-        men: { status: "ok", rating: 4.5, stalls: 2 },
-        women: { status: "ok", rating: 4.7, stalls: 3 },
+        men: { status: "ok", rating: 4.4, stalls: 6, urinals: 3  },
+        women: { status: "ok", rating: 4.7, stalls: 7 },
         gn: { status: "ok", rating: 4.8, stalls: 1 }
       },
       "2": {
-        men: { status: "ok", rating: 4.5, stalls: 2 },
-        women: { status: "ok", rating: 4.7, stalls: 3 },
-        gn: { status: "ok", rating: 4.8, stalls: 1 }
+        men: { status: "ok", rating: 3.8, stalls: 1, urinals: 2  },
+        women: { status: "ok", rating: 4.7, stalls: 4 },
       },
       "3": {
-        men: { status: "ok", rating: 4.2, stalls: 3 },
-        women: { status: "broken", rating: 4.0, stalls: 4 },
+        men: { status: "ok", rating: 4.5, stalls: 3,urinals: 3  },
+        women: { status: "broken", rating: 4.0, stalls: 6},
         gn: { status: "cleaning", rating: 4.4, stalls: 1 }
       },
       "4": {
-        men: { status: "ok", rating: 4.2, stalls: 3 },
-        women: { status: "broken", rating: 4.0, stalls: 4 },
+        men: { status: "ok", rating: 4.0, stalls:2 , urinals: 2 },
+        women: { status: "broken", rating: 4.0, stalls: 5 },
         gn: { status: "cleaning", rating: 4.4, stalls: 1 }
       }
     }
